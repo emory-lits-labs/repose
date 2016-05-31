70fd2902123d2b8cb41e0fd30cc0468caffe54fd
rlskoeser <rebecca.s.koeser@emory.edu>
2016-05-31 16:02:57 -0400

    Make stats tables sortable with jquery datatable

--- repose/settings.py
+++ repose/settings.py
@@ -120,7 +120,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'sitemedia'),   # eventually, probably
+    os.path.join(BASE_DIR, 'sitemedia'),
 ]
 
 
